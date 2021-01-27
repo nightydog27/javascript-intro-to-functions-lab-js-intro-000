4b81f73d0cb05771aee4ead6066a5ee9f7412c34
Jon C <nightydog27@hotmail.com>
2021-01-27 06:00:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log();
+}
+
+function logWhisper(string) {
+  console.log();
+}
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma = string.toLowerCase)
+  returns("I can\'t hear you!")
+}
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma = string.toUpperCase)
+  returns("YES INDEED")
+}
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma = "I love you, Grandma.")
+  returns("I love you, too.");
+}
